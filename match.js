5d259ecc861f323f985a49fe71eb219b0d43ec10
KirstenJoy <47281068+KirstenJoy@users.noreply.github.com>
2020-07-04 18:59:01 +0100

    Uploading the finding match feature

--- match.js
+++ match.js
@@ -1,13 +1,19 @@
 //Math method and if else statment / Love Challenge
 var first = prompt("Enter your Name 1");
-var second = prompt("Enter your Name 2")
+var slices = first.slice(0, 1).toUpperCase();
+var slices1 = first.slice(1, 35).toLowerCase();
+var result = slices + slices1;
+var second = prompt("Enter your Name 2");
+var slices2 = second.slice(0, 1).toUpperCase();
+var slices3 = second.slice(1, 35).toLowerCase();
+var results = slices2 + slices3;
 var num = (Math.random() * 100) + 1;
 var rounded = Math.round(num);
 
 if (rounded < 40) {
-    alert(first + " and " + second + " have " + rounded + "% , You don't love each other")
+    alert(result + " and " + results + " have " + rounded + "% , You don't love each other")
 } else if (rounded >= 40 && rounded <= 70) {
-    alert(first + " and " + second + " have " + rounded + "%, It's a match")
+    alert(result + " and " + results + " have " + rounded + "%, It's a match")
 } else {
-    alert(first + " and " + second + " have " + rounded + "%, You  love each other")
+    alert(result + " and " + results + " have " + rounded + "%, You  love each other")
 }
